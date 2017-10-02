375a5bc58b864e559f9e7055f4c55d2cae2f4141
Alex Jankuv <ajankuv@macbook-pro.local>
2017-10-02 19:50:05 -0400

    adding subcalls

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 
 python3
+
 python-dev
+
 pip install pip install wiringpi
--- hydrocloud.py
+++ hydrocloud.py
@@ -12,7 +12,15 @@ def index():
 def cakes():
     return render_template('main.html')
 
+#setup calls for hardware to set state
 subprocess.call(['./gpio-out.sh'])
+wiringpi.wiringPiSetupGpio()
+
+def pin0on():
+    subprocess.call(['gpio write 0 1'])
+
+def pin0off():
+    subprocess.call(['gpio write 0 0'])    
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=4200)
