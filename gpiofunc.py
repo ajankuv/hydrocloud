9f85d7de8ca0cd258adaab1abc7d4e1810603f08
Alex Jankuv <ajankuv@macbook-pro.local>
2017-10-03 14:31:28 -0400

    setting up route files

--- gpiofunc.py
+++ gpiofunc.py
@@ -5,11 +5,11 @@ on = Blueprint('on', __name__)
 off = Blueprint('off', __name__)
 
 @on.route('/on')
-def turn_on():
+def on():
     subprocess.call(['gpio', 'write', '0', '1'])
     return '', 204  # no content
 
 @off.route('/off')
-def turn_off():
+def off():
     subprocess.call(['gpio', 'write', '0', '0'])
     return '', 204  # no content
