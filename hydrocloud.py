faa051688ff74a6d7249ecf79d433e77270929ca
Alex Jankuv <ajankuv@macbook-pro.local>
2017-10-02 21:40:21 -0400

    adding subcalls

--- hydrocloud.py
+++ hydrocloud.py
@@ -14,12 +14,12 @@ def main():
 
 @app.route('/on')
 def turn_on():
-    subprocess.call(['./gpio', 'write', '0', 1])
+    subprocess.call(['./gpio', 'write', '0', '1'])
     return '', 204  # no content
 
 @app.route('/off')
 def turn_off():
-    subprocess.call(['./gpio', 'write', '0', 0])
+    subprocess.call(['./gpio', 'write', '0', '0'])
     return '', 204  # no content
 
 
