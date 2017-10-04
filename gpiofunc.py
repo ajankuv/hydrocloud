89ffeac64f6101c8e817bcc604b03452b05bc1a0
Alex Jankuv <ajankuv@macbook-pro.local>
2017-10-04 12:12:45 -0400

    adding args for on-off function

--- gpiofunc.py
+++ gpiofunc.py
@@ -6,10 +6,10 @@ off = Blueprint('off', __name__)
 
 @on.route('/on/<gpiopin>')
 def turn_on(gpiopin):
-    subprocess.call(['gpio', 'write', view_args['gpiopin'], '1'])
+    subprocess.call(['gpio', 'write', request.view_args['gpiopin'], '1'])
     return '', 204  # no content
 
 @off.route('/off/<gpiopin>')
 def turn_off(gpiopin):
-    subprocess.call(['gpio', 'write', view_args['gpiopin'], '0'])
+    subprocess.call(['gpio', 'write', request.view_args['gpiopin'], '0'])
     return '', 204  # no content
