fce2e56f6e3837259d28aa986523b5406fc4ac2c
Alex Jankuv <ajankuv@macbook-pro.local>
2017-10-08 13:53:25 -0400

    revert

--- hydrocloud.py
+++ hydrocloud.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 from gpiofunc import *
 import subprocess
 import hydrocloudlogger
-from routes import *
+
 
 app = Flask(__name__)
 app.register_blueprint(on)
 app.register_blueprint(off)
-pp.register_blueprint(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/main')
+def main():
+    return render_template('overview.html')
 
 #set state for all relays to off
 subprocess.call(['./scripts/gpio-out.sh'])
