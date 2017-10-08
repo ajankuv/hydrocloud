feb85ecd3117239362c214fcecd0cdcbf0bc575f
Alex Jankuv <ajankuv@macbook-pro.local>
2017-10-08 13:50:34 -0400

    moving routes to class file

--- hydrocloud.py
+++ hydrocloud.py
@@ -7,7 +7,7 @@ import routes
 app = Flask(__name__)
 app.register_blueprint(on)
 app.register_blueprint(off)
-
+pp.register_blueprint(app)
 
 #set state for all relays to off
 subprocess.call(['./scripts/gpio-out.sh'])
--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app = Blueprint('app', __name__)
 
 @app.route('/')
 def index():
